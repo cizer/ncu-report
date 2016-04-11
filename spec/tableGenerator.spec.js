98b5371c2d86e11625cffb83c3f051c146683c6d
Richie Mackay <richie.mackay@jandltech.com>
2016-04-11 23:44:58 +0100

    update tests and output for new column

--- spec/tableGenerator.spec.js
+++ spec/tableGenerator.spec.js
@@ -27,49 +27,40 @@ describe("format", function() {
         name: "outdated1",
         version: {
           current: 1,
+          installed: 1.5,
           new: 2
         }
       }, {
         name: "outdated2",
         version: {
           current: 2,
+          installed: 2.5,
           new: 3
         }
       }, {
         name: "ok1",
         version: {
           current: 4,
-          new: 4
-        }
-      }, {
-        name: "nodata1",
-        version: {
-          current: 4,
-          new: undefined
+          installed: 4
         }
       }]);
     });
 
     it("return table header", function() {
-      expect(result[0]).toBe("| Package | Current | Latest | Status |");
+      expect(result[0]).toBe("| Package | Current | Installed | Latest | Status |");
     });
 
     it("returns valid formatted table item (line1)", function() {
-      expect(result[2]).toBe("| outdated1 | 1 | 2 | OUTDATED |");
+      expect(result[2]).toBe("| outdated1 | 1 | 1.5 | 2 | OUTDATED |");
     });
 
     it("returns valid formatted table item (line2)", function() {
-      expect(result[3]).toBe("| outdated2 | 2 | 3 | OUTDATED |");
+      expect(result[3]).toBe("| outdated2 | 2 | 2.5 | 3 | OUTDATED |");
     });
 
     it("returns valid formatted table item (line3)", function() {
-      expect(result[4]).toBe("| ok1 | 4 | 4 | OK |");
+      expect(result[4]).toBe("| ok1 | 4 | 4 | 4 | OK |");
     });
-
-    it("returns valid formatted table item (line4)", function() {
-      expect(result[5]).toBe("| nodata1 | 4 | - | NO DATA |");
-    });
-
   });
 
 });
